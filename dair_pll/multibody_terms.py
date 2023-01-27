af9865724efcb47446d7b9c2707f9c3570606c7a
Bibit Bianchini <ebianchini.mit@gmail.com>
2023-01-27 16:32:35 -0500

    Perform inertia editing in a more physically feasible way

--- dair_pll/multibody_terms.py
+++ dair_pll/multibody_terms.py
@@ -166,7 +166,7 @@ class LagrangianTerms(Module):
 
         # store the original inertial parameters since not all will be learned
         self.original_pi_cm_params = InertialParameterConverter.theta_to_pi_cm(
-                                    self.inertial_parameters)
+                                    self.inertial_parameters.detach())
 
         # keep track of what inertial parameters will or will not be learned
         self.inertia_mode_txt = INERTIA_PARAM_OPTIONS[inertia_mode]
@@ -179,32 +179,59 @@ class LagrangianTerms(Module):
         curr_pi_cm = InertialParameterConverter.theta_to_pi_cm(
                     self.inertial_parameters)
 
-        # Reminder, pi format is:
+        # Reminder, pi_cm format is:
         # [m, m * p_x, m * p_y, m * p_z, I_xx, I_yy, I_zz, I_xy, I_xz, I_yz]
 
         # Overwrite any inertial parameters that should not be learned.  In all
         # cases, overwrite the mass of the first object to handle scale
         # invariance.
         orig = self.original_pi_cm_params
-        curr_pi_cm[0,0] = orig[0,0].item()
+        orig_m = orig[0,0].item()
+        curr_m = curr_pi_cm[0,0].item()
 
+        # Explicitly overwrite the mass of the first link, since this value is
+        # unobservable.  All of the remaining inertial parameters are
+        # detectable when this value is fixed.  Use direct assignment to zero
+        # the parameter gradient.
+        curr_pi_cm[0,0] = orig_m
+
+        # All of the other inertial parameters of the first link are
+        # proportional to its mass, so ensure physical consistency by scaling
+        # the paraemters appropriately.  Use multiplication to preserve the
+        # parameter gradients.
+        curr_pi_cm[0,1:] *= orig_m/curr_m
+
+        # Overwrite more parameters based on the inertia mode.
         mode = self.inertia_mode_txt
-
-        # Overwrite the moments of inertia unless learning all remaining
-        # parameters.
         if mode != 'all':
-            curr_pi_cm[:, 4:10] = orig[:, 4:10].detach()
+            # Overwrite the moments of inertia unless learning all remaining
+            # parameters.  Since moments of inertia scale with mass, ensure 
+            # physical feasibility by scaling the parameters with the current
+            # mass belief and not the original mass (which may differ).  Use
+            # direct assignment to zero the parameter gradient.
+            curr_pi_cm[:, 4:] = (orig[:, 4:].T * \
+                                 curr_pi_cm[:, 0].detach() / orig[:, 0]).T
 
-        # Overwrite the masses unless learning those.
-        if (mode == 'none') or ('masses' not in mode):
-            curr_pi_cm[:, 0] = orig[:, 0].detach()
+            # Overwrite the masses unless learning those.
+            if (mode == 'none') or ('masses' not in mode):
+                # Scale the rest of the parameters appropriately.  Use
+                # multiplication to preserve the parameter gradients.
+                curr_pi_cm[:, 1:] *= (orig[:, 0] / \
+                                      curr_pi_cm[:, 0].detach()).unsqueeze(1)
 
-        # Overwrite the center of masses unless learning those.
-        if (mode == 'none') or ('CoMs' not in mode):
-            # pi format has the center of masses multiplied by the mass, so
-            # need to use the new updated mass (which might equal old mass).
-            curr_pi_cm[:, 1:4] = (orig[:, 1:4].detach().T * \
-                               curr_pi_cm[:, 0] / orig[:, 0].detach()).T
+                # Overwrite the masses.  Use direct assignment to zero the
+                # parameter gradients.
+                curr_pi_cm[:, 0] = orig[:, 0]
+
+            # Overwrite the center of masses unless learning those.
+            if (mode == 'none') or ('CoMs' not in mode):
+                # pi format has the center of masses multiplied by the mass, so
+                # ensure physical feasibility by scaling the parameters with the
+                # current mass belief and not the original mass (which may
+                # differ).  Use direct assignment to zero the parameter
+                # gradient.
+                curr_pi_cm[:, 1:4] = (orig[:, 1:4].T * \
+                                      curr_pi_cm[:, 0].detach() / orig[:, 0]).T
 
         # convert back to inertial parameters
         return InertialParameterConverter.pi_cm_to_theta(curr_pi_cm)
