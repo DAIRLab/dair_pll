cd309bf69550f6af586993c154b0a0e4d6912793
ebianchi <ebianchini.mit@gmail.com>
2023-03-21 23:35:31 +0000

    Fix startup bash script so it runs on cluster (neowise/chiron)

--- examples/startup.bash
+++ examples/startup.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gpus=0
 #SBATCH --mem-per-cpu=10G
-#SBATCH --qos=np-med
+#SBATCH --qos=mp-med
 #SBATCH --partition=posa-compute
 #SBATCH --account mp-account
 #SBATCH --time=12:00:00
@@ -9,15 +9,13 @@
 #SBATCH --output={pll_dir}/logs/slurm_{name}.txt
 
 echo "display" >> {pll_dir}/logs/start_{name}.txt
-#Xvfb :6 -screen 0 800x600x24 &
 source {pll_dir}/pll_env/bin/activate;
 export PYTHONPATH={pll_dir};
 export PLL_EXPERIMENT={name};
-export DISPLAY=:5;
 
 
 echo "repo at hash {hash}" >> {pll_dir}/logs/start_{name}.txt
 
 echo "train" >> {pll_dir}/logs/start_{name}.txt
-PYTHONUNBUFFERED=1 python {pll_dir}/examples/contactnets_simple.py {name} {train_args} >> {pll_dir}/logs/train_{name}.txt
+PYTHONUNBUFFERED=1 xvfb-run --server-num="$SLURM_JOBID" --server-args="-screen 0 800x600x24" python {pll_dir}/examples/contactnets_simple.py {name} {train_args} >> {pll_dir}/logs/train_{name}.txt
 
