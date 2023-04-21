62101c28d01f445b8095d2d9b2102811bc0c6a6d
Bibit Bianchini <ebianchini.mit@gmail.com>
2023-04-20 20:22:20 -0400

    Include helper script to find hyperparameters from W&B IDs

---
+++ helpers/find_wandb_group.py
@@ -0,0 +1,66 @@
+"""Helper script to determine the run parameters from the W&B group ID."""
+import os
+import os.path as op
+import git
+import fnmatch
+import pdb
+import csv
+
+
+ELBOW_HP_SCRIPT_PATTERN = 'startup_hyperparam_elbow_he????.bash'
+
+FIELDNAMES = ['wandb_id', 'run name', 'loss variation', 'w_comp', 'w_diss',
+			  'w_pen']
+
+
+repo = git.Repo(search_parent_directories=True)
+git_folder = repo.git.rev_parse("--show-toplevel")
+git_folder = op.normpath(git_folder)
+
+startup_scripts_folder = op.join(git_folder, 'examples')
+
+startup_scripts_list = sorted(os.listdir(startup_scripts_folder))
+
+
+def get_params_from_bash_script(script_name):
+	full_script_path = f'{startup_scripts_folder}/{script_name}'
+	script = open(full_script_path, 'r').read()
+	wandb_id = script.split('WANDB_RUN_GROUP=')[-1].split(';')[0]
+	loss_variation = script.split('loss-variation=')[-1].split(' ')[0]
+	w_comp = script.split('w-comp=')[-1].split(' ')[0]
+	w_diss = script.split('w-diss=')[-1].split(' ')[0]
+	w_pen = script.split('w-pen=')[-1].split('\n')[0]
+
+	return {'wandb_id': wandb_id,
+			'loss variation': loss_variation,
+			'w_comp': w_comp,
+			'w_diss': w_diss,
+			'w_pen': w_pen}
+
+
+lookup_by_wandb_id = {}
+lookup_by_run_name = {}
+
+for script in startup_scripts_list:
+	if fnmatch.fnmatch(script, ELBOW_HP_SCRIPT_PATTERN):
+		run_name = script.split('_')[-1].split('.')[0]
+		params_dict = get_params_from_bash_script(script)
+		params_dict['run name'] = run_name
+		wandb_id = params_dict['wandb_id']
+
+		print(run_name, wandb_id)
+
+		lookup_by_wandb_id[wandb_id] = params_dict
+		lookup_by_run_name[run_name] = params_dict
+
+
+
+with open('hyperparameter_lookup.csv', 'w', newline='') as csvfile:
+	writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
+
+	writer.writeheader()
+	for run_info in lookup_by_wandb_id.keys():
+		writer.writerow(lookup_by_wandb_id[run_info])
+
+
+pdb.set_trace()
