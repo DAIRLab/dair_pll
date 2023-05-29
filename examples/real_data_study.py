4b736bea1559094c7b4327d38d841017c30211d7
Mathew Halm <mshalm95@gmail.com>
2023-05-29 11:48:55 -0400

    Prevent unnecessary file overwrites

--- examples/real_data_study.py
+++ examples/real_data_study.py
@@ -40,7 +40,7 @@ IMPORT_DIRECTORY = file_utils.get_asset(CUBE_DATA_ASSET)
 
 # Optimization configuration.
 LR = Float(1e-4, (1e-6, 1e-2), log=True)
-WD = Float(0.0, (0.0, 0.0), log=False)
+WD = Float(1e-6, (1e-10, 1e-2), log=False)
 EPOCHS = 100
 PATIENCE = 5
 BATCH_SIZE = Int(32, (1, 256), log=True)
@@ -57,24 +57,23 @@ MIN_RESOURCES = 5
 
 WANDB_PROJECT = 'contactnets-journal-results'
 
-StudyParams = Tuple[bool,
-                    Optional[MeshRepresentation],
-                    Optional[MultibodyLosses],
-                    str]
+StudyParams = Tuple[bool, Optional[MeshRepresentation],
+                    Optional[MultibodyLosses], str]
 DEEP_LEARNED_STUDY_PARAMS = (False, None, None, 'MLP')
-DEEP_DIFF_PHYS_STUDY_PARAMS = (True,
-                               MeshRepresentation.DEEP_SUPPORT_CONVEX,
+DEEP_DIFF_PHYS_STUDY_PARAMS = (True, MeshRepresentation.DEEP_SUPPORT_CONVEX,
                                MultibodyLosses.PREDICTION_LOSS,
                                'DEEP_DIFF_PHYS')
-DEEP_CONTACTNETS_STUDY_PARAMS = (True,
-                                 MeshRepresentation.DEEP_SUPPORT_CONVEX,
+DEEP_CONTACTNETS_STUDY_PARAMS = (True, MeshRepresentation.DEEP_SUPPORT_CONVEX,
                                  MultibodyLosses.CONTACTNETS_ANITESCU_LOSS,
                                  'DEEP_CONTACTNETS')
-STUDY_PARAMS = cast(List[StudyParams], [
-    DEEP_LEARNED_STUDY_PARAMS,
-    #DEEP_DIFF_PHYS_STUDY_PARAMS,
-    #DEEP_CONTACTNETS_STUDY_PARAMS,
-])
+STUDY_PARAMS = cast(
+    List[StudyParams],
+    [
+        DEEP_LEARNED_STUDY_PARAMS,
+        #DEEP_DIFF_PHYS_STUDY_PARAMS,
+        #DEEP_CONTACTNETS_STUDY_PARAMS,
+    ])
+
 
 def study_name_from_params(params: StudyParams) -> str:
     return f'{STUDY_NAME_PREFIX}_{params[3]}'
@@ -100,9 +99,7 @@ def main(sweep_num: int) -> None:
         urdfs=URDFS, initial_parameter_noise_level=PARAMETER_NOISE_LEVEL)
 
     deep_learnable_config = DeepLearnableSystemConfig(
-        model_constructor=MLP,
-        integrator_type=DeltaVelocityIntegrator
-    )
+        model_constructor=MLP, integrator_type=DeltaVelocityIntegrator)
 
     slice_config = TrajectorySliceConfig()
     train_valid_test_quantities = (N_HYPERPARMETER_OPTIMIZATION_TRAIN,
@@ -120,8 +117,10 @@ def main(sweep_num: int) -> None:
     hyperparameter_mask[hyperparameter_trajectories] = True
     torch.random.set_rng_state(torch_random_generator_state)
 
-    file_utils.import_data_to_storage(STORAGE,
-                                      import_data_dir=IMPORT_DIRECTORY)
+    # Import data.
+    if not os.path.exists(IMPORT_DIRECTORY):
+        file_utils.import_data_to_storage(STORAGE,
+                                          import_data_dir=IMPORT_DIRECTORY)
 
     data_config = DataConfig(
         dt=DT,
@@ -152,13 +151,13 @@ def main(sweep_num: int) -> None:
         data_config=data_config,
         full_evaluation_period=1,
         run_wandb=True,
-        wandb_project=WANDB_PROJECT
-    )
+        wandb_project=WANDB_PROJECT)
 
     for study_params in STUDY_PARAMS:
         if study_params[0]:
-            default_experiment_config = deepcopy(
-                multibody_experiment_config)
+            assert study_params[1] is not None
+            assert study_params[2] is not None
+            default_experiment_config = deepcopy(multibody_experiment_config)
             assert isinstance(default_experiment_config.learnable_config,
                               MultibodyLearnableSystemConfig)
             default_experiment_config.learnable_config.mesh_representation = (
