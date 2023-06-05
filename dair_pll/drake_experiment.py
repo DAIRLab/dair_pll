f59312e294df6b50f8987cd385c808eb5fb31b48
Bibit Bianchini <ebianchini.mit@gmail.com>
2023-06-05 18:56:47 -0400

    Fix prediction loss error with new regularization terms

--- dair_pll/drake_experiment.py
+++ dair_pll/drake_experiment.py
@@ -472,7 +472,7 @@ class DrakeMultibodyLearnableExperiment(DrakeExperiment):
                 "Don't recognize more than three regularization terms.")
         elif len(regularizers) == 3:
             reg_term = (regularizers[0] * w_res) + \
-                       (regularizers[1] * w_res_w) \
+                       (regularizers[1] * w_res_w) + \
                        (regularizers[2] * 1e-5)
         else:
             reg_term = torch.zeros_like(prediction_loss)
