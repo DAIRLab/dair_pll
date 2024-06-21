4dbac07f9babb88bfaa2d0cc928f088555d50882
Ethan K. Gordon <ethankg@seas.upenn.edu>
2024-06-21 15:23:39 -0400

    Update deps and drake_utils for v1.28.0 of pydrake and modern versions of pip

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from pip._internal import main as pipmain
 
 install_reqs = [
     # library
@@ -8,7 +9,7 @@ install_reqs = [
     'wandb',
     'mujoco-py',
     'optuna',
-    'numpy',
+    'numpy==1.26.4',
     'scipy',
     'typing_extensions',
     'matplotlib',
@@ -21,8 +22,7 @@ install_reqs = [
     'cvxpylayers',
     # documentation
     'networkx',
-    'protobuf==3.20.*',
-    'pydeps==1.10.12',
+    'pydeps',
     'Sphinx',
     'sphinx-autodoc-typehints',
     'sphinx-rtd-theme',
@@ -32,24 +32,25 @@ install_reqs = [
     'yapf',
     'pylint',
     'mypy',
+    # git
+    'drake-pytorch @ git+https://github.com/DAIRLab/drake-pytorch.git#egg=drake-pytorch-0.1',
+    'sappy @ git+https://github.com/mshalm/sappy.git#egg=sappy-0.0.1',
 ]
 
 try:
+    # Note: pydrake needs numpy, install numpy manually first
+    try:
+        import numpy
+    except ModuleNotFoundError as e:
+        pipmain(['install', 'numpy'])
     import pydrake
-
     print('USING FOUND DRAKE VERSION')
 except ModuleNotFoundError as e:
     install_reqs += ['drake']
-
-dependency_links = [
-    'git+https://github.com/DAIRLab/drake-pytorch.git',
-    'git+https://github.com/mshalm/sappy.git'
-]
 
 setup(
     name='dair_pll',
     version='0.0.1',
     packages=['dair_pll'],
     install_requires=install_reqs,
-    dependency_links=dependency_links
 )
